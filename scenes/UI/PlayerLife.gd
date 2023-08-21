28bb6e41458abb45def530277c293e91d008f6ae
otaviohneves <otahaen@gmail.com>
2023-08-21 14:26:03 -0300

    Updated Player, Enemy and Tiles visuals

--- scenes/UI/PlayerLife.gd
+++ scenes/UI/PlayerLife.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 func take_damage():
 	var lifes = $MarginContainer/LifesContainer.get_children()
 	var last_life = lifes.pop_back()
-	last_life.queue_free()
+	last_life.queue_free ()
 
 func _unhandled_input(event):
 	if (event.is_action_pressed("jump")):
